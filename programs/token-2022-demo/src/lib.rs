ea8fe86d77782e484b5eeb58a206a9899b10d05f
viet tu <quyen.luminous@gmail.com>
2022-11-07 19:04:45 +0700

    (+) test some fee token

--- programs/token-2022-demo/src/lib.rs
+++ programs/token-2022-demo/src/lib.rs
@@ -1,5 +1,5 @@
 use anchor_lang::prelude::*;
-
+use anchor_spl::token::TokenAccount;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
@@ -7,6 +7,15 @@ pub mod token_2022_demo {
     use super::*;
 
     pub fn create_token_pool(ctx: Context<CreateTokenPool>) -> Result<()> {
+        let token_pool = &mut ctx.accounts.token_pool;
+        token_pool.index = 0;
+        token_pool.fee_collector_address = *ctx.accounts.signer.to_account_info().key;
+        Ok(())
+    }
+
+    pub fn registry_token(ctx: Context<RegistryToken>) -> Result<()> {
+        let token_pool = &mut ctx.accounts.token_pool;
+        token_pool.index += 1;
 
         Ok(())
     }
@@ -17,9 +26,20 @@ pub struct CreateTokenPool<'info> {
     #[account(
     init,
     payer=signer,
-    space= 4 + 32
+    space= 8 + 4 + 32
     )]
     pub token_pool: Account<'info, TokenPool>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    pub system_program: Program<'info,System>,
+}
+
+#[derive(Accounts)]
+pub struct RegistryToken<'info> {
+    #[account(mut)]
+    pub token_pool: Account<'info, TokenPool>,
+    #[account(mut)]
+    pub token_key: Account<'info, TokenAccount>,
     #[account(mut)]
     pub signer: Signer<'info>,
     pub system_program: Program<'info,System>,
--- tests/token-2022-demo.ts
+++ tests/token-2022-demo.ts
@@ -1,16 +1,78 @@
 import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
-import { Token2022Demo } from "../target/types/token_2022_demo";
+import {Program} from "@project-serum/anchor";
+import {Token2022Demo} from "../target/types/token_2022_demo";
+import {SystemProgram} from "@solana/web3.js";
+import {assert} from "chai";
+import {
+    ExtensionType,
+    createInitializeMintInstruction,
+    mintTo,
+    createAccount,
+    getMintLen,
+    TOKEN_2022_PROGRAM_ID, createInitializeTransferFeeConfigInstruction,
+} from '@solana/spl-token';
 
 describe("token-2022-demo", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+    // Configure the client to use the local cluster.
+    const provider = anchor.AnchorProvider.env();
 
-  const program = anchor.workspace.Token2022Demo as Program<Token2022Demo>;
+    anchor.setProvider(provider);
+    const program = anchor.workspace.Token2022Demo as Program<Token2022Demo>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
+    let pool;
+    let feeCollector;
+    let token1;
+    let feeConfig;
+    let withdrawWithheldAuthority;
+    let mint;
+    let mintAuthority;
+
+    const extensions = [ExtensionType.TransferFeeConfig];
+
+    const mintLen = getMintLen(extensions);
+    const decimals = 9;
+    const feeBasisPoints = 50;
+    const maxFee = BigInt(5_000);
+
+    it("Is initialized!", async () => {
+        feeCollector = anchor.web3.Keypair.generate();
+        pool = anchor.web3.Keypair.generate();
+        const tx = await program.rpc.createTokenPool(
+            {
+                accounts: {
+                    tokenPool: pool.publicKey,
+                    signer: provider.wallet.publicKey,
+                    systemProgram: SystemProgram.programId
+                },
+                signers: [pool],
+            }
+        );
+        const tokenPoolInfo = await program.account.tokenPool.fetch(pool.publicKey);
+        assert.equal(tokenPoolInfo.feeCollectorAddress.toString(), provider.wallet.publicKey.toString())
+    });
+
+    it("create a token fee", async () => {
+        mint = anchor.web3.Keypair.generate();
+
+        mintAuthority = anchor.web3.Keypair.generate();
+        const mintLamports = await provider.connection.getMinimumBalanceForRentExemption(mintLen);
+        SystemProgram.createAccount({
+            fromPubkey: provider.wallet.publicKey,
+            newAccountPubkey: mint,
+            space: mintLen,
+            lamports: mintLamports,
+            programId: TOKEN_2022_PROGRAM_ID,
+        })
+        feeConfig = anchor.web3.Keypair.generate();
+        withdrawWithheldAuthority = anchor.web3.Keypair.generate();
+        createInitializeTransferFeeConfigInstruction(
+            mint.publicKey,
+            feeConfig.publicKey,
+            withdrawWithheldAuthority.publicKey,
+            feeBasisPoints,
+            maxFee,
+            TOKEN_2022_PROGRAM_ID
+        )
+        createInitializeMintInstruction(mint, decimals, mintAuthority.publicKey, null, TOKEN_2022_PROGRAM_ID)
+    })
 });
